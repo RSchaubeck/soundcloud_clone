96f5b909e5a081118921dcb74bfdace0f124b459
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-04-06 14:48:57 -0400

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+
+    validates :username, :password_digest, :session_token, presence: true, uniqueness: true 
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil unless user && user.is_password?(password)
+        user
+    end
+
+    def is_password?(password)
+        bc_pw = BCrypt::Password.new(self.password_digest)
+        bc_pw.is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.update!(session_token: User.generate_session_token)
+        self.session_token
+    end
+end
