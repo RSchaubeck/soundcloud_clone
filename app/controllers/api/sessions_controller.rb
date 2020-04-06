3a5ecd66716fb06dc4230e21c96d033d1d7fcb5f
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-04-06 15:34:32 -0400

    created sessionController & added ensure_logged_out method to application controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SessionsController < ApplicationController
+    before_action :ensure_logged_in, only: [:destroy]
+    before_action :ensure_logged_out, only: [:create]
+
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user
+            login(@user)
+            render "api/users/show"
+        else
+            render json: ["Invalid username/password"], status: 422
+        end
+    end
+
+    def destroy
+        @user = current_user
+        if @user
+            logout
+            render "api/users/show"
+        else
+            render json: ["Not logged in!"], status: 404
+        end
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
         redirect_to new_session_url unless logged_in?
     end
 
+    def ensure_logged_in
+        redirect_to root_url if logged_in?
+    end
+
     def logged_in?
         !!current_user
     end
