4e9326f40e0d8f0995cfdbc8799a70ff09e5b162
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-04-08 12:12:11 -0400

    styled the navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -5,17 +5,25 @@ const Navbar = ({ currentUser, logout, openModal }) => {
     if (currentUser) {
         return(
             <header className="navbar-container" >
-                <NavLink to="/" className="logo">JaeCloud</NavLink>
-                <NavLink to="/">{currentUser.username}</NavLink>              
-                <button className="logout-button" onClick={logout}>Log Out</button>
+                <div className="left-nav">
+                    <NavLink to="/" className="logo"><i className="fab fa-mixcloud"></i>JaeCloud</NavLink>
+                </div>
+                <div className="right-nav">
+                    <NavLink to="/" className="profile-button nav-button">{currentUser.username}</NavLink>              
+                    <button className="logout-button nav-button" onClick={logout}>Log Out</button>
+                </div>
             </header>
         )
     } else {
         return(
             <header className="navbar-container">
-                <NavLink to="/" className="logo">JaeCloud</NavLink>
-                <button className="login-button" onClick={() => openModal('login')}>Sign In</button>
-                <button className="signup-button" onClick={() => openModal('signup')}>Create Account</button>
+                <div className="left-nav">
+                    <NavLink to="/" className="logo"><i className="fab fa-mixcloud"></i>JaeCloud</NavLink>
+                </div>
+                <div className="right-nav">
+                    <button className="login-button nav-button" onClick={() => openModal('login')}>Sign In</button>
+                    <button className="signup-button nav-button" onClick={() => openModal('signup')}>Create Account</button>
+                </div>
             </header>
         )
     }
